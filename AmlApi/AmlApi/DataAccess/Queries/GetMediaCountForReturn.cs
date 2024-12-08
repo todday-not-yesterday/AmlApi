5a7d7f83a2e66b3fb5fcac0c2a212948c20ab50d
lawrence_whalley4 <lawrence@churchoverhouse.co.uk>
2024-12-08 11:49:26 +0000

    version++

--- AmlApi/AmlApi/DataAccess/Queries/GetMediaCountForReturn.cs
+++ AmlApi/AmlApi/DataAccess/Queries/GetMediaCountForReturn.cs
@@ -1,7 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using AmlApi.DataAccess.Enums;
 using AmlApi.DataAccess.Queries.Interfaces;
 using AmlApi.Resources;
+using Microsoft.EntityFrameworkCore;
 
 namespace AmlApi.DataAccess.Queries;
 
@@ -20,7 +22,11 @@ public class GetMediaCountForReturn : IGetMediaCount
     {
         using (var context = dataContextFactory.Create())
         {
-            return 0;
+            return await context.Inventories
+                .Where(x => (filters.MediaTypes == null || !filters.MediaTypes.Any() || filters.MediaTypes.Contains(x.MediaType.Key))
+                            && ((filters.Branches == null || !filters.Branches.Any()) || filters.Branches.Contains(x.Branch.Key))
+                            && (filters.SearchItem == null || x.Name.ToLower().Contains(filters.SearchItem.ToLower())))
+                .CountAsync();
         }
     }
 }
--- AmlApi/AmlApi/DataAccess/Queries/GetMediaForReturn.cs
+++ AmlApi/AmlApi/DataAccess/Queries/GetMediaForReturn.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AmlApi.DataAccess.Entities;
 using AmlApi.DataAccess.Enums;
 using AmlApi.DataAccess.Queries.Interfaces;
 using AmlApi.Resources;
+using Microsoft.EntityFrameworkCore;
 
 namespace AmlApi.DataAccess.Queries;
 
@@ -22,7 +24,56 @@ public class GetMediaForReturn : IGetFilteredMedia
     {
         using (var context = dataContextFactory.Create())
         {
-            return null;
+            if (filters.Branches.Any())
+            {
+                return await context.Inventories
+                    .Include(x => x.MediaType)
+                    .Include(x => x.Branch)
+                    .Where(x => filters.Branches.Contains(x.BranchKey))
+                    .Skip(filters.PageSize * filters.PageNumber - filters.PageSize)
+                    .Take(filters.PageSize)
+                    .ToListAsync();
+            }
+
+            if (filters.MediaTypes.Any())
+            {
+                return await context.Inventories
+                    .Include(x => x.MediaType)
+                    .Include(x => x.Branch)
+                    .Where(x => filters.MediaTypes.Contains(x.MediaTypeKey))
+                    .Skip(filters.PageSize * filters.PageNumber - filters.PageSize)
+                    .Take(filters.PageSize)
+                    .ToListAsync();
+            }
+
+            if (filters.SearchItem != null)
+            {
+                return await context.Inventories
+                    .Include(x => x.MediaType)
+                    .Include(x => x.Branch)
+                    .Where(x => filters.SearchItem.Contains(x.Name))
+                    .Skip(filters.PageSize * filters.PageNumber - filters.PageSize)
+                    .Take(filters.PageSize)
+                    .ToListAsync();
+            }
+
+            if (filters.PublicationYear != null)
+            {
+                return await context.Inventories
+                    .Include(x => x.MediaType)
+                    .Include(x => x.Branch)
+                    .Where(x => x.PublicationYear == filters.PublicationYear)
+                    .Skip(filters.PageSize * filters.PageNumber - filters.PageSize)
+                    .Take(filters.PageSize)
+                    .ToListAsync();
+            }
+
+            return await context.Inventories
+                .Include(x => x.MediaType)
+                .Include(x => x.Branch)
+                .Skip(filters.PageSize * filters.PageNumber - filters.PageSize)
+                .Take(filters.PageSize)
+                .ToListAsync();
         }
     }
 }

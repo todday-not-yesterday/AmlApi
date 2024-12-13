a08822b53d7d4521a4a4e78c788c828ce5fc0bd9
DESKTOP-13NKDGA\Josh <joshtodd0026@gmail.com>
2024-12-13 18:58:26 +0000

    Added anoither unit test for transfering

--- AmlApi/Tests/Processors/TransferDataProcessorTests.cs
+++ AmlApi/Tests/Processors/TransferDataProcessorTests.cs
@@ -69,6 +69,60 @@ public class TransferDataProcessorTests : DbTestBase<TransferDataProcessor>
     [Fact]
     public async void ProcessMediaDoesNotExistsInTransferToBranch()
     {
-        
+        using (var stubContext = this.CreateAppDataContext())
+        {
+            var stubTransferData = new TransferData()
+            {
+                Key = 1,
+                Branch = 1,
+                StockLevel = 10
+            };
+            
+            var stubToBeTransferred = new Inventory()
+            {
+                Key = 1,
+                Name = "name",
+                MediaTypeKey = 1,
+                BranchKey = 1,
+                Author = "John Doe",
+                PublicationYear = 2012,
+                StockLevel = 20
+            };
+
+            var stubTransferringTo = new Inventory()
+            {
+                Key = 20,
+                Name = "name",
+                MediaTypeKey = 1,
+                BranchKey = 1,
+                Author = "John Doe",
+                PublicationYear = 2012,
+            };
+
+            this.AutoMocker.GetMock<IDataContextFactory>()
+                .Setup(x => x.Create())
+                .Returns(stubContext);
+
+            this.AutoMocker.GetMock<IGetByKeyQuery>()
+                .Setup(x => x.Get<Inventory>(stubTransferData.Key))
+                .ReturnsAsync(stubToBeTransferred);
+
+            this.AutoMocker.GetMock<IGetMediaByNameAndBranchQuery>()
+                .Setup(x => x.Execute(stubContext, stubToBeTransferred.Name, stubTransferData.Branch))
+                .ReturnsAsync(stubTransferringTo);
+
+            stubTransferringTo.StockLevel += stubTransferData.StockLevel;
+
+            var sut = this.CreateTestSubject();
+            var result = await sut.Process(stubTransferData);
+            
+            Assert.Equal("Transferred Media Successfully", result);
+            
+            this.AutoMocker.GetMock<IInsertEntityCommand>()
+                .Verify(x => x.Execute(It.IsAny<Inventory>()), Times.Never);
+            
+            this.AutoMocker.GetMock<IUpdateEntityCommand>()
+                .Verify(x => x.Execute(It.IsAny<Inventory>()), Times.Exactly(2));
+        }
     }
 }
